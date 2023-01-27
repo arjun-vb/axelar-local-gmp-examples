abba9c93c9b1922266133eedd80ac88abe4a7031
arjun-vb <arjun157@ucsb.edu>
2023-01-27 09:20:08 -0800

    updated

--- examples/cross-chain/Coordinator.sol
+++ examples/cross-chain/Coordinator.sol
@@ -2,27 +2,59 @@
 
 pragma solidity 0.8.9;
 
-contract Coordinator { 
+import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executables/AxelarExecutable.sol';
+import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
+import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+
+contract Coordinator is AxelarExecutable { 
 
     enum State { PUBHLISHED, REDEEM, REFUND }
+
     uint constant participantCount = 2;
     string[] participantAddress;
     string[] participantChain;
-    bool verified = false;
     mapping(string => bool) public confirmation;
     mapping(string => bool) public funded;
-    State currState = State.PUBHLISHED;
 
-    function registerParticipants(string calldata sourceAddress_, string calldata sourceChain_) public {
+    State currState = State.PUBHLISHED;
+    bool verified = false;
+
+    IAxelarGasService public immutable gasReceiver;
+
+    constructor(address gateway_, address gasReceiver_) AxelarExecutable(gateway_) {
+        gasReceiver = IAxelarGasService(gasReceiver_);
+    }
+    string public value;
+    function _execute(
+        string calldata sourceChain_,
+        string calldata sourceAddress_,
+        bytes calldata payload_
+    ) internal override {
+        (value) = abi.decode(payload_, (string));
+        if(stringEquals(value, "REGISTER")) {
+            registerParticipants(sourceAddress_, sourceChain_);
+        } else if(stringEquals(value, "CONFIRM")) {
+            confirmVerification(sourceAddress_);
+        }
+    }
+    function stringEquals(string memory first, string memory second) internal pure returns(bool) {
+        if(keccak256(abi.encodePacked(first)) == keccak256(abi.encodePacked(second))) {
+            return true;
+        }
+        return false;
+
+    }
+
+    function registerParticipants(string memory sourceAddress_, string memory sourceChain_) public {
         if(participantAddress.length < 2) {
             participantAddress.push(sourceAddress_);
             participantChain.push(sourceChain_);
         }
     }
 
-    function manualVerification(string calldata sourceAddress_) public {
+    function confirmVerification(string memory sourceAddress_) public {
         for(uint i = 0; i < participantCount; i++) {
-            if(keccak256(abi.encodePacked(participantAddress[i])) == keccak256(abi.encodePacked(sourceAddress_))) {
+            if(stringEquals(participantAddress[i], sourceAddress_)) {
                 confirmation[sourceAddress_] = true;
                 break;
             }
@@ -30,7 +62,7 @@ contract Coordinator {
     }
     function fundConfirmation(string calldata sourceAddress_) public {
         for(uint i = 0; i < participantCount; i++) {
-            if(keccak256(abi.encodePacked(participantAddress[i])) == keccak256(abi.encodePacked(sourceAddress_))) {
+            if(stringEquals(participantAddress[i], sourceAddress_)) {
                 funded[sourceAddress_] = true;
                 break;
             }
--- examples/cross-chain/Replica.sol
+++ examples/cross-chain/Replica.sol
@@ -14,62 +14,57 @@ contract Replica is AxelarExecutable {
     enum MyState { INITIAL, REDEEMED, REFUNDED }
 
     address payable owner;
-    uint public total_funds;
-    bool public funded;
-    uint constant public minFunds = 30;
+    uint public total_funds = 0;
+    bool public funded = false;
     MyState state = MyState.INITIAL;
 
-    address constant public coordinatorAddress = 0xFBC3B76A206f03f1edbF411F280444cD3fD9c7C8;
-    string constant public coordinatorChain = "ETHEREUM";
-    address constant public recieverAddress = 0xFBC3B76A206f03f1edbF411F280444cD3fD9c7C8;
+    uint public immutable fundsToTransfer;
+    string public coordinatorAddress;
+    string public coordinatorChain;
+    string public recieverAddress;
     
 
-    constructor(address gateway_, address gasReceiver_) AxelarExecutable(gateway_) {
+    constructor(
+        address gateway_, 
+        address gasReceiver_, 
+        string memory coordinatorAddress_,
+        string memory coordinatorChain_,
+        string memory recieverAddress_,
+        uint fundsToTransfer_
+    ) AxelarExecutable(gateway_) {
         gasReceiver = IAxelarGasService(gasReceiver_);
-        //owner = payable(msg.sender);
+        coordinatorAddress = coordinatorAddress_;
+        coordinatorChain = coordinatorChain_;
+        recieverAddress = recieverAddress_;
+        fundsToTransfer = fundsToTransfer_;
+        owner = payable(msg.sender);
     }
 
-    function register(
-        string calldata destinationChain, 
-        string calldata destinationAddress
-    ) external payable {
-        funded = false;
-        bytes memory payload = abi.encode("REGISTER");
+    function register() external {
+        sendCoordinator("REGISTER");
+    }
+
+    function sendConfirmation() external {
+        sendCoordinator("CONFIRM");
+    }
+
+    function sendCoordinator ( string memory message_ ) public payable {
+        bytes memory payload = abi.encode(message_);
         if (msg.value > 0) {
             gasReceiver.payNativeGasForContractCall{ value: msg.value }(
                 address(this),
-                destinationChain,
-                destinationAddress,
+                coordinatorChain,
+                coordinatorAddress,
                 payload,
                 msg.sender
             );
         }
-        gateway.callContract(destinationChain, destinationAddress, payload);
-        //register address with coordinator
-    }
-
-    function sendConfirmation(
-        string calldata destinationChain, 
-        string calldata destinationAddress
-    ) external payable{
-        bytes memory payload = abi.encode("CONFIRM");
-        if (msg.value > 0) {
-            gasReceiver.payNativeGasForContractCall{ value: msg.value }(
-                address(this),
-                destinationChain,
-                destinationAddress,
-                payload,
-                msg.sender
-            );
-        }
-        gateway.callContract(destinationChain, destinationAddress, payload);
-        
-        //send confirmation to coordinator
+        gateway.callContract(coordinatorChain, coordinatorAddress, payload);
     }
 
     function recieveFunds() public payable {
         total_funds += msg.value;
-        if(total_funds >= minFunds) {
+        if(total_funds >= fundsToTransfer) {
             funded = true;
             //send fund confirmation
         }
@@ -77,9 +72,9 @@ contract Replica is AxelarExecutable {
 
     function redeem() public {
         CoordinatorState coordinatorState = CoordinatorState.PUBHLISHED; //check coordinator status 
-        if(coordinatorState == CoordinatorState.REDEEM && address(this).balance >= minFunds && state == MyState.INITIAL) {
-            payable(recieverAddress).transfer(minFunds);
-            //(bool sent, ) = payable(recieverAddress).call{value: minFunds}("");
+        if(coordinatorState == CoordinatorState.REDEEM && address(this).balance >= fundsToTransfer && state == MyState.INITIAL) {
+            //payable(recieverAddress).transfer(fundsToTransfer);
+            //(bool sent, ) = payable(recieverAddress).call{value: fundsToTransfer}("");
             //require(sent, "Failure!");
             state = MyState.REDEEMED;
         }
@@ -94,5 +89,22 @@ contract Replica is AxelarExecutable {
             state = MyState.REFUNDED;
         }
     }
+
+    // function bytesToAddress (bytes memory b) internal returns (address) {
+    //     uint result = 0;
+    //     for (uint i = 0; i < b.length; i++) {
+    //         uint c = uint(b[i]);
+    //         if (c >= 48 && c <= 57) {
+    //             result = result * 16 + (c - 48);
+    //         }
+    //         if(c >= 65 && c<= 90) {
+    //             result = result * 16 + (c - 55);
+    //         }
+    //         if(c >= 97 && c<= 122) {
+    //             result = result * 16 + (c - 87);
+    //         }
+    //     }
+    //     return address(result);
+    // }
 
 }
